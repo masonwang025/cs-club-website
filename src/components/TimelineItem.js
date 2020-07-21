7dee695f5109275c7996ca39c32ce1cb0a98e1e0
MasonWang025 <mason.wang0025@gmail.com>
2020-07-21 12:15:23 -0700

    fade in effect

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+import Fade from "react-reveal/Fade";
+
 export default function TimelineItem(props) {
   return (
-    <div className="t-container">
-      <div className="content">
-        <h2>{props.title}</h2>
-        {props.unit && <span>{props.unit}</span>}
-        {props.children}
+    <Fade duration={1550}>
+      <div className="t-container">
+        <div className="content">
+          <h2>{props.title}</h2>
+          {props.unit && <span>{props.unit}</span>}
+          {props.children}
+        </div>
       </div>
-    </div>
+    </Fade>
   );
 }
