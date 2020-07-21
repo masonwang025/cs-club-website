463772e1d15de7221240e006674c11e949352a6e
MasonWang025 <mason.wang0025@gmail.com>
2020-07-21 13:16:30 -0700

    coloring for about page

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -5,10 +5,11 @@ import Fade from "react-reveal/Fade";
 export default function TimelineItem(props) {
   return (
     <Fade duration={1550}>
-      <div className="t-container">
+      <div className={`t-container ${props.unit.toLowerCase()}`}>
         <div className="content">
           <h2>{props.title}</h2>
-          {props.unit && <span>{props.unit}</span>}
+          {props.unit && <span className="unit">{props.unit}</span>}
+          <span> {props.date && <small>~ {props.date}</small>}</span>
           {props.children}
         </div>
       </div>
--- src/pages/Curriculum.js
+++ src/pages/Curriculum.js
@@ -10,24 +10,32 @@ export default function Curriculum() {
       </div>
       <div className="main-container">
         <div className="timeline">
-          <TimelineItem title="Example Title" unit="Algorithms">
-            <p>Content goes here</p>
-          </TimelineItem>
-          <TimelineItem title="Example Title" unit="Algorithms">
+          <TimelineItem
+            title="Algorithms Unit Start"
+            unit="Algorithms"
+            date="9/14/20"
+          >
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Perferendis in necessitatibus nulla eius id velit dolores tempore
-              aut recusandae esse.
+              Having <b>fast code</b> can mean the{" "}
+              <b>difference between taking several years and a few seconds</b>{" "}
+              to find the fastest route to your house on Google Maps. Algorithms
+              such as Djikstra’s shortest path algorithm and quicksort are
+              responsible for speeding up code by finding clever ways to reduce
+              computations.
             </p>
           </TimelineItem>
-          <TimelineItem title="Example Title" unit="Algorithms">
+          <TimelineItem
+            title="Intro to Algorithms"
+            unit="Algorithms"
+            date="9/14/20"
+          >
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Perferendis in necessitatibus nulla eius id velit dolores tempore
-              aut recusandae esse.
+              <b>Motivation behind algorithm study and big O analysis.</b>
+              <br />
+              Activity: Fibonacci recursion analysis and knapsack example.
             </p>
           </TimelineItem>
-          <TimelineItem title="Example Title" unit="Algorithms">
+          <TimelineItem title="Systems Unit Start" unit="Systems">
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Perferendis in necessitatibus nulla eius id velit dolores tempore
