9c405ba79f990bb5d973533fc41a0352bb2cd462
MasonWang025 <mason.wang0025@gmail.com>
2020-07-21 18:08:51 -0700

    fit more into theme

--- src/components/UnitLink.js
+++ src/components/UnitLink.js
@@ -4,16 +4,14 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 export default function UnitLink({ name }) {
   let escapedName = name.replace(/\s+/g, "-").toLowerCase();
   return (
-    <div className={`unit-link ${escapedName}`}>
-      <AnchorLink
-        offset={() => {
-          if (window.innerWidth <= 768) return 70;
-          else return 100;
-        }}
-        href={`#${escapedName}-start`}
-      >
-        {name}
-      </AnchorLink>
-    </div>
+    <AnchorLink
+      offset={() => {
+        if (window.innerWidth <= 768) return 70;
+        else return 100;
+      }}
+      href={`#${escapedName}-start`}
+    >
+      <div className={`unit-link ${escapedName}`}>{name}</div>
+    </AnchorLink>
   );
 }
