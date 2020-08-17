41ea04138c322661ef35402af22a501eff5e54e2
MasonWang025 <mason.wang0025@gmail.com>
2020-08-17 11:10:39 -0700

    filtering works

---
+++ src/components/competitionsList.js
@@ -0,0 +1,14 @@
+const competitionsList = [
+  {
+    name: "USACO",
+    description: "Change this placeholder description.",
+    tags: ["competitive programming"],
+  },
+  {
+    name: "Example Contest",
+    description: "Example description here.",
+    tags: ["tag 1", "another tag"],
+  },
+];
+
+export default competitionsList;
--- src/pages/Competitions.js
+++ src/pages/Competitions.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import allContests from "../components/competitionsList";
 
 export default function Competitions() {
+  const [contestList, updateContestList] = useState(allContests);
+
+  useEffect(() => {
+    window.setTimeout(() => {
+      document.getElementById("search-filter").focus();
+    }, 0); // why this works i don't know either
+  });
+
   return (
-    <div className="page">
-      <div className="fcontainer">
-        <div className="row">
-          <h3>This is the competitions page</h3>
-        </div>
+    <div className="competitions page" id="top">
+      <div className="info">
+        <h1>Recommended Competitions</h1>
+        <h2>The greatest events to test your skills.</h2>
+        <input
+          type="text"
+          placeholder="Search by name or topic"
+          id="search-filter"
+          onChange={(e) => {
+            let filterText = e.target.value.toLowerCase().trim();
+            updateContestList(
+              allContests.filter((contest) => {
+                for (let i = 0; i < contest.tags.length; i++)
+                  if (contest.tags[i].toLowerCase().trim().includes(filterText))
+                    return true;
+
+                return contest.name.toLowerCase().trim().includes(filterText);
+              })
+            );
+          }}
+        ></input>
+        {contestList.map((contest) => (
+          <div key={contest.name} className="contest-card card">
+            <div className="card-title">{contest.name}</div>
+            <div className="card-body">
+              {contest.tags.map((tag) => (
+                <div key={contest.name + tag} className="tag">
+                  {tag}
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
