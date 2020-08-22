2280cbb8e8bc4a16e1d21b44d94c6beb9c7c6a98
Rohan Kumar <rohan@heberway.com>
2020-08-22 14:43:13 -0700

    fix warnings

--- src/pages/listItems.js
+++ src/pages/listItems.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import AssistantPhotoIcon from '@material-ui/icons/AssistantPhoto';
 import BarChartIcon from '@material-ui/icons/BarChart';
-import AssignmentIcon from '@material-ui/icons/Assignment';
 
 export const mainListItems = (
     <div>
