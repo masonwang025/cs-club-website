d7964a637a60198653db49c63b1e4f0460e6d0e8
Rohan Kumar <rohan@heberway.com>
2020-08-23 13:20:31 -0700

    fix warnings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
